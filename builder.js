562b649604b5ac7e1bae847bd3eda9fdc861b864
Luke Edwards <luke.edwards05@gmail.com>
2018-12-24 01:04:24 -0800

    chore: update builder

--- builder.js
+++ builder.js
@@ -1,6 +1,6 @@
 const fs = require('fs');
 const mkdir = require('mk-dirs');
-const { minify } = require('uglify-es');
+const { minify } = require('terser');
 const imports = require('rewrite-imports');
 const pretty = require('pretty-bytes');
 const sizer = require('gzip-size');
@@ -18,7 +18,7 @@ mkdir('dist').then(_ => {
 	fs.writeFileSync(pkg.main, data);
 
 	// Uglify & print gzip size
-	const { code } = minify(data);
+	const { code } = minify(data, { toplevel:true });
 	const int = sizer.sync(code);
 	console.log(`> gzip size: ${pretty(int)}`);
 });
