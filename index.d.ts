2cfeed638e0169343af35e302539f51388efe313
Luke Edwards <luke.edwards05@gmail.com>
2021-05-23 14:21:46 -0700

    feat: add `inject` function

--- index.d.ts
+++ index.d.ts
@@ -7,3 +7,16 @@ export function parse(route: RegExp): {
 	keys: false;
 	pattern: RegExp;
 }
+
+export type RouteParams<T extends string> =
+	T extends `${string}:${infer P}?/${infer Rest}`
+		? { [K in P]?: string } & RouteParams<Rest>
+	: T extends `${string}:${infer P}/${infer Rest}`
+		? { [K in P]: string } & RouteParams<Rest>
+	: T extends `${string}:${infer P}?`
+		? { [K in P]?: string }
+	: T extends `${string}:${infer P}`
+		? { [K in P]: string }
+	: {};
+
+export function inject<T extends string>(route: T, values: RouteParams<T>): string;
